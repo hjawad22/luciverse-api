4a2eacb2f06f2fe13794369cca8c71fb75233de5
Houda Jawad <houdajawad@gmail.com>
2026-01-10 17:28:40 -0800

    Sets url for database production

--- db/knex.js
+++ db/knex.js
@@ -1,13 +1,16 @@
-const environment = process.env.NODE_ENV || 'development';
-console.log('NODE_ENV:', environment);
+const environment =
+  process.env.NODE_ENV ||
+  (process.env.DATABASE_URL ? 'production' : 'development');
+
+console.log('NODE_ENV resolved to:', environment);
+
 const config = require('./knexfile')[environment];
 
 if (!config) {
-  throw new Error(`Knex configuration for environment "${environment}" not found!`);
+  throw new Error(`Knex configuration for environment "${environment}" not found`);
 }
 
-console.log('Using DB config:', config);
+console.log('Using knex config:', environment);
 
 const knex = require('knex')(config);
-
 module.exports = knex;
